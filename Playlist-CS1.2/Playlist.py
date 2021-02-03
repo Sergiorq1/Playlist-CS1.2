40601018dbcf34fab343e23aae0c1404db225e0a
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2021-02-02 21:41:01 -0800

    add song and length method in playlist.py

--- Playlist-CS1.2/Playlist.py
+++ Playlist-CS1.2/Playlist.py
@@ -8,14 +8,33 @@ class Playlist:
   # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
   def add_song(self, title):
-    pass
+    # New song instance (node)
+    new_song = Song(title)
 
+    # Setting title
+    new_song.set_title(title)
 
+    # Assigns next song to the head
+    new_song.set_next_song(self.__first_song)
+
+    # head point at new song
+    self.__first_song = new_song
 
   # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
-    pass
+    song_counter = 0
+    current_song = self.__first_song
+
+    while current_song.get_title() != title:
+      #Counter is index, as it moves through the linked list, so does the counter
+      song_counter += 1 
+      #Goes to next element
+      current_song = current_song.get_next_song()
+
+      if current_song.get_title() == title:
+        #Returns iteration
+        return song_counter
 
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
@@ -28,7 +47,12 @@ class Playlist:
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    pass
+    song_counter = 0
+    current_song = self.__first_song
+    while current_song != None:
+      song_counter += 1 
+      current_song = current_song.get_next_song()
+    return song_counter
 
 
   # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
